6f3c6d858b466bc19da0f6821aa9bcb518a9ff27
Mohamed Askar <moasker20@gmail.com>
2026-08-12 09:46:59 +0300

    chore: add data structures

--- Banking-System-Demo/scr/header-files/core/core.h
+++ Banking-System-Demo/scr/header-files/core/core.h
@@ -1,3 +1,14 @@
 #pragma once
+#include <iostream>
 
 void PrintMainMnueScreen();
+
+struct stClientData
+{
+	std::string accNumber = "";
+	std::string PinCode = "";
+	std::string name = "";
+	std::string phone = "";
+	double accBalance = 0;
+	bool MarkForDelete = false;
+};
