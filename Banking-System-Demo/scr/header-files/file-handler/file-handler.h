5cd7377410b1e8b7b97cb1f1510b1f89741fc436
Mohamed Askar <moasker20@gmail.com>
2026-08-12 11:09:28 +0300

    fix: minor issue

--- Banking-System-Demo/scr/header-files/file-handler/file-handler.h
+++ Banking-System-Demo/scr/header-files/file-handler/file-handler.h
@@ -7,4 +7,4 @@ std::vector <std::string> split(std::string line, const std::string& delimiter);
 
 stClientData ConvertClientDataLineToRecord(std::string& DataLine, const std::string& delimiter);
 
-std::vector <stClientData> LoadDataFromFileToVector(const std::string& FileName, const std::string& delimiter)
+std::vector <stClientData> LoadDataFromFileToVector(const std::string& FileName, const std::string& delimiter);
