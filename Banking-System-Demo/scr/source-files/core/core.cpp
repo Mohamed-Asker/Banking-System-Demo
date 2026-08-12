3b483cdf00df1f9d001033a1a1dd8a0bf70da52e
Mohamed Askar <moasker20@gmail.com>
2026-08-12 08:36:43 +0300

    feat: added main mnue screen

---
+++ Banking-System-Demo/scr/source-files/core/core.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include "../../header-files/core/core.h"
+
+
+void PrintHeaderMainMnue()
+{
+	std::cout << "\n\n";
+	printf("%*s", 87, "-----------------------------------------------------\n");
+	printf("%*s", 76, "* * *  MAIN MNUE SCREEN * * * \n");
+	printf("%*s", 87, "-----------------------------------------------------\n");
+}
+
+void PrintMainMnueScreen()
+{
+	PrintHeaderMainMnue();
+	std::cout << std::left << std::setw(33) << "" << "[1] Show clients list.\n";
+	std::cout << std::left << std::setw(33) << "" << "[2] Add new clietn.\n";;
+	std::cout << std::left << std::setw(33) << "" << "[3] Delete client.\n";;
+	std::cout << std::left << std::setw(33) << "" << "[4] Update client.\n";
+	std::cout << std::left << std::setw(33) << "" << "[5] Find client.\n";;
+	std::cout << std::left << std::setw(33) << "" << "[6] Exit.\n";
+	printf("%*s", 87, "-----------------------------------------------------\n");
+}
