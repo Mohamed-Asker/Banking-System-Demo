f8c5f103f1569537d8c66e3286e5a64089c1e31e
Mohamed Askar <moasker20@gmail.com>
2026-08-14 08:41:08 +0300

    wip: implment transactions mnue

--- Banking-System-Demo/scr/source-files/core/core.cpp
+++ Banking-System-Demo/scr/source-files/core/core.cpp
@@ -14,11 +14,29 @@ void PrintHeaderMainMnue()
 void PrintMainMnueScreen()
 {
 	PrintHeaderMainMnue();
-	std::cout << std::left << std::setw(33) << "" << "[1] Show clients list.\n";
-	std::cout << std::left << std::setw(33) << "" << "[2] Add new clietn.\n";;
-	std::cout << std::left << std::setw(33) << "" << "[3] Delete client.\n";;
-	std::cout << std::left << std::setw(33) << "" << "[4] Update client.\n";
-	std::cout << std::left << std::setw(33) << "" << "[5] Find client.\n";;
-	std::cout << std::left << std::setw(33) << "" << "[6] Exit.\n";
+	std::cout << std::left << std::setw(width) << "" << "[1] Show Clients List.\n";
+	std::cout << std::left << std::setw(width) << "" << "[2] Add New Clietn.\n";;
+	std::cout << std::left << std::setw(width) << "" << "[3] Delete Client.\n";;
+	std::cout << std::left << std::setw(width) << "" << "[4] Update Client.\n";
+	std::cout << std::left << std::setw(width) << "" << "[5] Find Client.\n";;
+	std::cout << std::left << std::setw(width) << "" << "[6] Exit.\n";
+	printf("%*s", 87, "-----------------------------------------------------\n");
+}
+
+void PrintHeaderTransactionsMnue()
+{
+	std::cout << "\n\n";
+	printf("%*s", 87, "-----------------------------------------------------\n");
+	printf("%*s", 80, "* * *  TRANSACTIONS MNUE SCREEN  * * * \n");
+	printf("%*s", 87, "-----------------------------------------------------\n");
+}
+
+void PrintTransactionsMnue()
+{
+	PrintHeaderTransactionsMnue();
+	std::cout << std::left << std::setw(width) << "" << "[1] Deposit.\n";
+	std::cout << std::left << std::setw(width) << "" << "[2] Withdraw.\n";
+	std::cout << std::left << std::setw(width) << "" << "[3] Total Balance.\n";
+	std::cout << std::left << std::setw(width) << "" << "[4] Main Mnue.\n";
 	printf("%*s", 87, "-----------------------------------------------------\n");
 }
