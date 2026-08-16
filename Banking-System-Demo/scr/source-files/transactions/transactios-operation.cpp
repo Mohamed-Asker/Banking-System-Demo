0d56d1007ac08f627716cc84c3cfb8c1c5165b2c
Mohamed Askar <moasker20@gmail.com>
2026-08-16 08:12:04 +0300

    refactor: improve ShowWithdrawScreen implementation

--- Banking-System-Demo/scr/source-files/transactions/transactios-operation.cpp
+++ Banking-System-Demo/scr/source-files/transactions/transactios-operation.cpp
@@ -24,6 +24,7 @@ void ShowDepositScreen(std::vector <stClientData>& vClients)
 	char answer;
 	std::cout << std::left << std::setw(width) << "" << "Are you sure you want perform  this transaction[Y,N]: ";
 	std::cin >> answer;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
 	if (std::tolower(answer) == 'y')
 	{
@@ -35,12 +36,33 @@ void ShowWithdrawScreen(std::vector <stClientData>& vClients)
 {
 	ResetScreen();
 	PrintHeaderWithdrawScreen();
+	stClientData client;
 	std::string accNumber = ReadAccountNumber();
-	while (!FindClientByAccountNumber(vClients, accNumber))
+	while (!FindClientByAccountNumber(vClients, accNumber,&client))
 	{
 		std::cout << std::left << std::setw(width) << "" << "Client with account number [" << accNumber << "] not exist.\n";
 		std::cout << std::left << std::setw(width) << "" << "Please enter another account number.\n\n";
 		accNumber = ReadAccountNumber();
 	}
-	DepositTransaction(vClients, accNumber, false);
+	ShowClientDetails(client);
+	std::cout << "\n";
+	double amount;
+	do
+	{
+		amount = ReadPositiveNumber("Enter withdraw amount", width);
+		if (amount > client.accBalance)
+		{
+			std::cout << std::left << std::setw(width) << "" << "Amount exceeds the balance.\n";
+			std::cout << std::left << std::setw(width) << "" << "You can withdraw up to: " << client.accBalance << "\n\n";
+		}
+	} while (amount > client.accBalance);
+	
+	char answer;
+	std::cout << std::left << std::setw(width) << "" << "Are you sure you want perform this transaction[Y,N]: ";
+	std::cin >> answer;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	if (std::tolower(answer) == 'y')
+	{
+		DepositTransaction(vClients, accNumber, amount * -1);
+	}
 }
